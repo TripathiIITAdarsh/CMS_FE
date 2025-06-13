9e19541e8bc6b40cc7951afd33034cd10fd92eb6
Adarsh Tripathi <adarshtripathi7118@gmail.com>
2025-06-13 23:38:27 +0530

    Initial commit of CMS_FE frontend

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+import postcss from '@tailwindcss/postcss';
+import autoprefixer from 'autoprefixer';
+
+/** @type {import('postcss').Config} */
+export default {
+  plugins: [postcss, autoprefixer],
+};
---
+++ src/pages/studentDashboard.tsx
@@ -0,0 +1,7 @@
+export default function studentDashboard() {
+  return (
+    <div className="text-center mt-10 text-4xl text-blue-600">
+      🏠 Welcome Student Dashboard
+    </div>
+  );
+}
